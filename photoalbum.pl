9cabb17d5516772e1b85490c403cfd2800ed061b
Leonardo Herrera <leonardo.herrera@gmail.com>
2009-12-26 00:30:59 -0300

    Using ExifTool to get the picture date, pictures by month, better layout handling

--- photoalbum.pl
+++ photoalbum.pl
@@ -1,10 +1,12 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use diagnostics;
 use Switch 'Perl6';
 
 use File::Find;
 use PDF::Create;
+use Image::ExifTool;
 
 sub get_images {
 	my @ret;
@@ -40,7 +42,8 @@ my $psz = $pdf->get_page_size($pagesize);
 print "psz: ", join(', ', @$psz), "\n";
 my $root = $pdf->new_page('MediaBox' => $psz);
 
-my @images = (get_images)[0..20];
+#my @images = (get_images)[0..40];
+my @images = get_images;
 my $page = $root->new_page;
 my ($accum, $y) = (0, 0);
 my ($pw, $ph) = (@$psz)[2,3];
@@ -81,11 +84,22 @@ sub fit_img {
 
 
 sub place_img {
-	my ($page, $img, $w, $h, $x, $y) = @_;
+	my ($page, $img, $w, $h, $slot_w, $slot_h, $x, $y) = @_;
+	my $margin = 10;
+	my $xscale;
 
-	my $xscale = $w / $$img{width};
+	if (abs($$img{width} - $w) > abs($$img{height} - $h)) {
+		$xscale = ($w - $margin * 2) / $$img{width};
+	} else {
+		$xscale = ($h - $margin * 2) / $$img{height};
 
-	print "Placing:  xpos => $x, ypos => $y\n";
+	}
+	my ($rw, $rh) = ($$img{width} * $xscale, $$img{height} * $xscale);
+
+	$x *= $slot_w;
+	$y *= $slot_h;
+	$x += ($slot_w - $rw) / 2;
+	$y += ($slot_h - $rh) / 2;
 
 	$page->image(image => $img,
 		xpos => $x,
@@ -95,6 +109,7 @@ sub place_img {
 	);
 }
 
+
 sub layout {
 	my ($page, $pw, $ph, @images) = @_;
 	my $max_width = max map { $$_{width} } @images;
@@ -103,26 +118,81 @@ sub layout {
 	my ($slot_w, $slot_h) = ($pw / $w, $ph / $h);
 
 	# margins
-	print "max: $max_width : $max_height - grid: $w x $h",
-	    " ($slot_w x $slot_h)\n";
 	my $margin = 20;
 
 	for (my $j = 0; $j < $h; $j++) {
 		for (my $i = 0; $i < $w; $i++) {
 			my $im = shift @images;
-			my ($w, $h) = fit_img($im, $slot_w, $slot_h);
-			print "orig: $$im{width} x $$im{height} - $w x $h\n";
-			place_img($page, $im, $w, $h, $i * $slot_w, $j * $slot_h);
+			if ($im) {
+				my ($w, $h) = fit_img($im, $slot_w, $slot_h);
+				place_img($page, $im,
+				    $w, $h,
+				    $slot_w, $slot_h,
+				    $i, $j);
+			}
 		}
 	}
 }
 
-while (@images) {
-	my $n = 6;
-	my @batch = map { $pdf->image($_) } splice @images, 0, $n;
-	my $page = $root->new_page;
+my @months = qw(Enero Febrero Marzo Abril
+    Mayo Junio Julio Agosto Septiembre
+    Octubre Noviembre Diciembre);
 
-	layout($page, $pw, $ph, @batch);
+my $f = $pdf->font('Subtype'  => 'Type1',
+	'Encoding' => 'WinAnsiEncoding',
+	'BaseFont' => 'Helvetica');
+
+my $exif = new Image::ExifTool;
+my %dt;
+sub get_pics_for_month {
+	my ($m, $pics) = @_;
+	my @results;
+	my $x;
+	foreach my $k (keys %$pics) {
+		print "$m - $k... ";
+		unless ($dt{$k}) {
+			my $info = $exif->ImageInfo($k, 'DateTimeOriginal');
+			die "info err"
+			    unless $info->{DateTimeOriginal} =~ /^(\d{4}):(\d\d):(\d\d) (\d\d):(\d\d)/;
+			$dt{$k} = {
+				y => $1, m => $2, d => $3, 
+				str => sprintf('%04d%02d%02d%02d%02d',
+				$1, $2, $3, $4, $5) };
+		}
+		if ($m == $dt{$k}->{m} - 1) {
+			print "matches.";
+			push @results, $k;
+			delete $pics->{$k};
+		}
+		print "\n";
+	}
+	return @results;
+}
+
+{
+my $month = 0;
+my %images = map { $_ => 1 } @images;
+while (keys %images) {
+	die "pics without date info?" if $month > 11;
+	my @for_this_month = 
+	    sort { $dt{$a}->{str} cmp $dt{$b}->{str} }
+		get_pics_for_month($month, \%images);
+	if (@for_this_month) {
+		print "Pics for this month: ", scalar @for_this_month, "\n";
+		$page = $root->new_page;
+		$page->stringc($f, 40, 306, 426, $months[$month]);
+		while (@for_this_month) {
+			my $n = 6;
+			my @to_proc = reverse splice @for_this_month, 0, $n;
+			my @batch = map { $pdf->image($_) } @to_proc; 
+			if (@batch) {
+				my $page = $root->new_page;
+				layout($page, $pw, $ph, @batch);
+			}
+		}
+	}
+	$month++;
+}
 }
 
 $pdf->close;
